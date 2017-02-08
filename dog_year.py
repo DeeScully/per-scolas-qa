a1ed5214b16d7c913d2f017c7b26b0346bb99bcd
wchu <wlanchu@gmail.com>
2017-02-08 18:20:36 -0500

    added dog_yr program

--- dog_year.py
+++ dog_year.py
@@ -1,5 +1,5 @@
 age_txt = input('Please input your dog\'s age in human years: ')
 age = float(age_txt)
 dog_age = age * 7
-print(dog_age)
+print('Your dog is %d years old in dog years!' %(dog_age))
 
